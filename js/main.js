1a0886930a5041136cc8a97e3b285be49ea1de07
Nicolas Botello <nbotell2@gmail.com>
2019-09-29 19:54:24 -0700

    remove isotope

--- js/main.js
+++ js/main.js
@@ -113,19 +113,6 @@
     time: 1000
   });
 
-  // Porfolio isotope and filter
-  $(window).on("load", function() {
-    var portfolioIsotope = $(".portfolio-container").isotope({
-      itemSelector: ".portfolio-item"
-    });
-    $("#portfolio-flters li").on("click", function() {
-      $("#portfolio-flters li").removeClass("filter-active");
-      $(this).addClass("filter-active");
-
-      portfolioIsotope.isotope({ filter: $(this).data("filter") });
-    });
-  });
-
   Handlebars.registerHelper("trimString", function(passedString) {
     var theString = passedString.substring(0, 150);
     return new Handlebars.SafeString(theString + "...");
